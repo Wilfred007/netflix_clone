ae27e077ebdf9d3e98cec7e447db85425a91d0bf
Wilfred Adzer <adzerwilfred007@gmail.com>
2023-06-20 16:09:38 +0100

    feat:sliders on the movie tabs

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { FaHeart, FaRegHeart} from 'react-icons/fa'
 import axios from 'axios'
+import Movie from './Movie'
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 const Row = ({title, fetchURL}) => {
     const [movies, setMovies] = useState ([])
-    const [like, setLike] = useState(false)
 
     useEffect(() => {
         axios.get(fetchURL).then((response) => {
@@ -18,23 +18,15 @@ const Row = ({title, fetchURL}) => {
   return (
     <>
         <h2 className='text-white font-bold sm:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center'>
-            <div id={'slider'}>
+        <div className='relative flex items-center group'>
+          <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+            <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                 {
-                    movies.map((items, id) => (
-                        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block curso:pointer relative p-2'>
-                          <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${items?.backdrop_path}`} alt={items?.title} />  
-                          <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white '>
-                            <p className='white-space-nornal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{items?.title}</p>
-                            <p>
-                              {like ? <FaHeart className='absolute top-4 left-4 text-gray-200' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-200' /> }
-                            </p>
-                          </div>
-                        </div>
-
-
+                    movies.map((item, id) => (
+                       <Movie key={id} item={item} />
                     ))}
             </div>
+            <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
         </div>
      </>
   )
